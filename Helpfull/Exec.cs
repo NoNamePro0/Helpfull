f8ad6bb0cafaab4738792a35b40bc47bb209754d
No Name Pro <noname.pro.nn@gmail.com>
2020-07-16 13:22:48 +0200

    ✨ Linux Bash

---
+++ Helpfull/Exec.cs
@@ -0,0 +1,36 @@
+﻿using Helpfull.Exeption;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Helpfull
+{
+    public class Exec
+    {
+        /// <summary>
+        /// Execute a Command on the Linux Bash.
+        /// </summary>
+        /// <param name="command">The Command witch will be executed.</param>
+        /// <returns>The String, that the Command returns.</returns>
+        public static string Bash(string command)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) { throw new IsNotLinuxExeption(); }
+
+            var escapedArgs = command.Replace("\"", "\\\"");
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
